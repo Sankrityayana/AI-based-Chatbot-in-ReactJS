b562c19d04988c6114c604dc3f52fdafcd8e6d81
Sankrityayana <shanbhagsuraj84@gmail.com>
2025-08-16 22:45:47 +0530

    Add Loader component and integrate react-markdown
    Introduced a new Loader component with associated styles. Updated dependencies to include react-markdown for rendering markdown content. Refactored Chat component to use Loader and react-markdown, and updated .env for API key format.

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { Chat } from './components/Chat/Chat';
 import { Assistant } from './assistants/googleai'; 
 import styles from './App.module.css';
 import { Controls } from './components/Controls/Controls';
+import { Loader } from './components/Loader/loader';
 
 function App() {
   const assistant = new Assistant();
   const [messages, setMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   function addMessage(message) {
     setMessages((prevMessages) => [...prevMessages, message]);
@@ -14,17 +16,21 @@ function App() {
 
   async function handleContentSend(content) {
     addMessage({ content, role: 'user' });
+    setIsLoading(true);
     try {
       const aireply  = await assistant.chat(content);
       addMessage({ content: aireply, role: 'assistant'});
     } catch (error) {
       console.error('Error generating content:', error);
       addMessage({ content: 'Sorry, I could not process your request. Please try again', role: 'system' });
+    } finally {
+      setIsLoading(false);
     }
   }
 
   return (
-    <div>
+    <div className={styles.App}>
+      {isLoading && <Loader />}
       <header className={styles.Header}>
       <img className={styles.Logo} src="/chatbot_logo.png" />
       <h2 className={styles.Title}>AI Chatbot</h2>
